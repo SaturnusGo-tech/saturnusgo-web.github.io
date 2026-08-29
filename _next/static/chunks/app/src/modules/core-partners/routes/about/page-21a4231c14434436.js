f2b3b10824939caef84eb5a63df1e66415a690b6
Mercury <m.ruks@domyland.com>
2026-08-29 03:00:24 +0300

    deploy: TMS 2a4871b7

---
+++ _next/static/chunks/app/src/modules/core-partners/routes/about/page-21a4231c14434436.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[8136],{83631:function(n,e,u){Promise.resolve().then(u.bind(u,91874))}},function(n){n.O(0,[29,750,1874,2971,2117,1744],function(){return n(n.s=83631)}),_N_E=n.O()}]);
