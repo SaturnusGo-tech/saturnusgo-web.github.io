d5113740326f629902d99ebba5551f30f88cdd1b
Mercury <m.ruks@domyland.com>
2026-09-04 22:21:22 +0300

    deploy: TMS 07ef7c84

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fc7bf402d0fbab4e"; }
+/******/ 		__webpack_require__.h = function() { return "1ce3a1af80e52ad8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
